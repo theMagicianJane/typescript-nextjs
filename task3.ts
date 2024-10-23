f6e84fcb656dc7bcb0210eae693e2dd98cfd75b0
ysharipbaeva <sharipbaeva92@gmail.com>
2024-10-23 17:49:47 +0200

    adjustments

--- task3.ts
+++ task3.ts
@@ -1,9 +1,11 @@
+const checkIfNumber = array => array.every(item => typeof item === 'number' && !isNaN(item));
+
 function getAverageValue (array: number[]): number {
-  if (!array || !array.length) {
+  if (!array || !array.length || !checkIfNumber(array)) {
     throw new Error('Arguments should include an array with items type number');
   }
 
-  return array.reduce((acc, value) => acc + value, 0)
+  return array.reduce((acc, value) => acc + value / array.length, 0)
 }
 
-console.log(getAverageValue([1, 2, 3]))
+console.log(getAverageValue([6, 1]))
