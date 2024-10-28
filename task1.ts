624a9a4704c921424f6aa8b11a62008379f9ec1f
ysharipbaeva <sharipbaeva92@gmail.com>
2024-10-28 11:58:25 +0100

    fixes

--- task1.ts
+++ task1.ts
@@ -1,9 +1,18 @@
-const arr: any[] = [1, 2, 3];
-const reverseArray = (values: any[]): any[] | never => {
+const arr: any[] = [1, 2, 3, 4, 5, 6];
+const reverseArray = values => {
+  let i = 0;
+
   if (!values && !Array.isArray(values)){
     throw new Error('An argument should be an array')
   }
-  return values.reverse();
+
+  while (i < values.length - 1) {
+    values.splice(i, 0, values[values.length - 1])
+    values.pop();
+
+    i++
+  }
+  return values;
 }
 
 const reversedArr = reverseArray(arr);
