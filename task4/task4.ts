0380bb1bdc6c999758d2ae1a314511408c992df5
ysharipbaeva <sharipbaeva92@gmail.com>
2024-10-30 18:39:12 +0100

    task 4

---
+++ task4/task4.ts
@@ -0,0 +1,21 @@
+const text = "This is a test, sentence test with some bad words.";
+const forbiddenWords = ["bad", "test"];
+
+const highlightForbiddenWords = (string: string, words: string[]): string => {
+  if (!words || !Array.isArray(words)) {
+    throw new Error('Argument words must be an array');
+  }
+
+  let newString: string = text;
+  const getRegExp = word => new RegExp(`\\b${word}\\b`,'gi');
+
+  words.forEach(word => {
+    newString = newString.replace(getRegExp(word),`<del>${word}</del>`);
+  });
+
+  return newString;
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById('text').innerHTML = highlightForbiddenWords(text, forbiddenWords);
+});
